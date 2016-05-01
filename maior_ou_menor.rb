8f6c5d56161ec376308052b3736ed95557457b61
rogeriop <pinheiroxrogerio@gmail.com>
2016-05-01 18:33:48 -0300

    fim 03 O poder da extracao de codigo

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,23 +1,52 @@
-puts "Bem vindo ao jogo de adivinhação"
-puts "Qual é o seu nome"
-nome = gets
-puts "\n" + "\n" + "\n"
+def da_boas_vindas
+	puts "Bem vindo ao jogo de adivinhacao"
+	puts "Qual eh o seu nome"
+	nome = gets
+	puts "\n\n\n"
+	puts "Comecaremos o jogo para voce, " + nome
+end
 
-puts "Começaremos o jogo para você, " + nome
+def sorteia_numero_secreto
+	puts "Escolhendo um numero secreto para voce entre 0 e 200 "
+	sorteio = 175
+	puts "Escolhido! Que tal tentar adivinha-lo?"
+	sorteio
+end
 
-puts "Escolhendo um número secreto para você entre 0 e 200 "
-numero_secreto = 175
+def pede_um_numero (tentativa, limite_de_tentativas)
+	puts "\n\n"
+	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts "Entre com o numero"
+	chute = gets
+	puts "Serah que acertou? Voce chutou " + chute
+	chute.to_i
+end
 
-puts "\n" + "\n" + "\n"
+def verifica_se_acertou(chute, numero_secreto)
+	acertou = chute == numero_secreto
+	if acertou
+		puts "Voce Acertou!"
+		return true
+	end
+	
+	maior = numero_secreto > chute
+	if maior
+		puts "O numero secreto eh maior"
+	else
+		puts "O numero secreto eh menor"
+	end	
+	false
+end
 
-puts "Tentativa um"
-puts "Entre com o numero"
-chute = gets
-puts "Serah que acertou? Voce chutou " + chute
-puts chute.to_i == numero_secreto
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
 
+puts "\n\n\n"
+limite_de_tentativas = 5
 
-
-
-
-
+for tentativa in 1..limite_de_tentativas
+	chute = pede_um_numero tentativa, limite_de_tentativas
+	if verifica_se_acertou chute, numero_secreto
+		break
+	end
+end
---
+++ teste.rb
@@ -0,0 +1,6 @@
+
+puts
+puts "Vou contar de 1 ateh 3"
+for x in 1..3
+	puts "Contando" + x.to_s
+end	
